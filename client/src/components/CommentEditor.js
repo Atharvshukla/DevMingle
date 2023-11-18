2dd2196cb0c775081fd92ecb7c82cec2fec713c9
JaivalBuchara <jaival.buchara2021@vitstudent.ac.in>
2023-11-18 23:41:57 +0530

    Comment improved.

--- client/src/components/CommentEditor.js
+++ client/src/components/CommentEditor.js
@@ -1,13 +1,115 @@
+// import { Button, Card, Stack, TextField, Typography } from "@mui/material";
+// import { Box } from "@mui/system";
+// import React, { useState } from "react";
+// import { Link, useNavigate, useParams } from "react-router-dom";
+// import { createComment } from "../api/posts";
+// import { isLoggedIn } from "../helpers/authHelper";
+// import ErrorAlert from "./ErrorAlert";
+// import HorizontalStack from "./util/HorizontalStack";
+
+// const CommentEditor = ({ label, comment, addComment, setReplying }) => {
+//   const [formData, setFormData] = useState({
+//     content: "",
+//   });
+
+//   const [error, setError] = useState("");
+//   const [loading, setLoading] = useState(false);
+
+//   const params = useParams();
+//   const navigate = useNavigate();
+
+//   const handleChange = (e) => {
+//     setFormData({ ...formData, [e.target.name]: e.target.value });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     const body = {
+//       ...formData,
+//       parentId: comment && comment._id,
+//     };
+
+//     setLoading(true);
+//     const data = await createComment(body, params, isLoggedIn());
+//     setLoading(false);
+
+//     if (data.error) {
+//       setError(data.error);
+//     } else {
+//       formData.content = "";
+//       setReplying && setReplying(false);
+//       addComment(data);
+//     }
+//   };
+
+//   const handleFocus = (e) => {
+//     !isLoggedIn() && navigate("/login");
+//   };
+
+//   return (
+//     <Card>
+//       <Stack spacing={2}>
+//         <HorizontalStack justifyContent="space-between">
+//           <Typography variant="h5">
+//             {comment ? <>Reply</> : <>Comment</>}
+//           </Typography>
+//           <Typography>
+//             <a href="https://commonmark.org/help/" target="_blank">
+//               Markdown Help
+//             </a>
+//           </Typography>
+//         </HorizontalStack>
+
+//         <Box component="form" onSubmit={handleSubmit}>
+//           <TextField
+//             multiline
+//             fullWidth
+//             label={label}
+//             rows={5}
+//             required
+//             name="content"
+//             sx={{
+//               backgroundColor: "white",
+//             }}
+//             onChange={handleChange}
+//             onFocus={handleFocus}
+//             value={formData.content}
+//           />
+
+//           <ErrorAlert error={error} sx={{ my: 4 }} />
+//           <Button
+//             variant="outlined"
+//             type="submit"
+//             fullWidth
+//             disabled={loading}
+//             sx={{
+//               backgroundColor: "white",
+//               mt: 2,
+//             }}
+//           >
+//             {loading ? <div>Submitting</div> : <div>Submit</div>}
+//           </Button>
+//         </Box>
+//       </Stack>
+//     </Card>
+//   );
+// };
+
+// export default CommentEditor;
+
+import React, { useState } from "react";
 import { Button, Card, Stack, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import React, { useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { createComment } from "../api/posts";
 import { isLoggedIn } from "../helpers/authHelper";
 import ErrorAlert from "./ErrorAlert";
 import HorizontalStack from "./util/HorizontalStack";
+import { useTheme } from "@mui/system"; // Add this import
 
 const CommentEditor = ({ label, comment, addComment, setReplying }) => {
+  const theme = useTheme(); // Use the useTheme hook
   const [formData, setFormData] = useState({
     content: "",
   });
@@ -48,20 +150,32 @@ const CommentEditor = ({ label, comment, addComment, setReplying }) => {
   };
 
   return (
-    <Card>
+    <Card
+      sx={{
+        backgroundColor: theme.palette.background.paper, // Use theme background color
+        borderRadius: theme.shape.borderRadius, // Use theme border radius
+        boxShadow: theme.shadows[2], // Use theme box shadow
+      }}
+    >
       <Stack spacing={2}>
         <HorizontalStack justifyContent="space-between">
-          <Typography variant="h5">
+          <Typography variant="h5" color={theme.palette.text.primary}> {/* Use theme text color */}
             {comment ? <>Reply</> : <>Comment</>}
           </Typography>
-          <Typography>
+          <Typography color={theme.palette.text.primary}> {/* Use theme text color */}
             <a href="https://commonmark.org/help/" target="_blank">
               Markdown Help
             </a>
           </Typography>
         </HorizontalStack>
 
-        <Box component="form" onSubmit={handleSubmit}>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{
+            backgroundColor: theme.palette.background.default, // Use theme background color
+          }}
+        >
           <TextField
             multiline
             fullWidth
@@ -70,7 +184,7 @@ const CommentEditor = ({ label, comment, addComment, setReplying }) => {
             required
             name="content"
             sx={{
-              backgroundColor: "white",
+              backgroundColor: theme.palette.background.paper, // Use theme background color
             }}
             onChange={handleChange}
             onFocus={handleFocus}
@@ -78,17 +192,22 @@ const CommentEditor = ({ label, comment, addComment, setReplying }) => {
           />
 
           <ErrorAlert error={error} sx={{ my: 4 }} />
+
           <Button
             variant="outlined"
             type="submit"
             fullWidth
             disabled={loading}
             sx={{
-              backgroundColor: "white",
+              backgroundColor: theme.palette.background.paper, // Use theme background color
               mt: 2,
             }}
           >
-            {loading ? <div>Submitting</div> : <div>Submit</div>}
+            {loading ? (
+              <div style={{ color: theme.palette.text.primary }}>Submitting</div>
+            ) : (
+              <div style={{ color: theme.palette.text.primary }}>Submit</div>
+            )}
           </Button>
         </Box>
       </Stack>
